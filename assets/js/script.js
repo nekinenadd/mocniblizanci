d2ef0535a1e019065d70cdd83d122014e8c9c892
nekinenadd <nenad.djurdjevic2002@gmail.com>
2025-06-11 18:20:48 +0200

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,77 @@
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+function preloadImages(imageUrls) {
+  imageUrls.forEach(url => {
+    const img = new Image();
+    img.src = url;
+  });
+}
+
+preloadImages([
+  './assets/galerija/galerija1.webp',
+  './assets/galerija/galerija2.webp',
+  './assets/galerija/galerija3.webp',
+  './assets/galerija/galerija4.webp',
+  './assets/galerija/galerija5.webp',
+  './assets/galerija/galerija6.webp',
+  './assets/galerija/galerija7.webp',
+  './assets/galerija/galerija8.webp',
+  './assets/galerija/galerija9.webp'
+]);
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNavbar = function () { navbar.classList.toggle("active"); }
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () { navbar.classList.remove("active"); }
+
+addEventOnElem(navLinks, "click", closeNavbar);
+
+
+
+/**
+ * header & back top btn active
+ */
+
+
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
